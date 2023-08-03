a7df085a35808b90fa2632c142b4469f38e99ed0
borussia444423 <jung78978978@naver.com>
2023-08-04 08:32:18 +0900

    호돌센세 도와주세요

---
+++ src/main/java/com/hodolog/api/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.hodolog.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "메인 페이지입니다.";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "사용자 페이지입니다.";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "관리자 페이지입니다.";
+    }
+}
---
+++ src/main/java/com/hodolog/api/domain/User.java
@@ -0,0 +1,38 @@
+package com.hodolog.api.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+
+    @Builder
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
+}
